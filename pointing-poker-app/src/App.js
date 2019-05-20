a9fc9bb9012265111770efb5056a4ad451771ed3
Kostadin Marinov <k.marinov@fio.de>
2019-05-20 17:55:29 +0300

    * add favicon * packages react-bootstrap and react-router-dom * main setup - index.html, index.js and css-es * navigation

--- pointing-poker-app/src/App.js
+++ pointing-poker-app/src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import Routes from "./Routes";
+import "./App.css";
 
-function App() {
-  const x = 1;
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <Navbar>
+          <Navbar.Brand>
+            <Link to="/">Home</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Item>
+                <Link to="/login">Login</Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes />
+      </div>
+    );
+  }
 }
 
 export default App;
